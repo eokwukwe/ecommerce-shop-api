22cb86c14c5fdd6cfbbed2d022cb522a6b6ee4ac
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2019-11-28 22:09:50 +0100

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -26,7 +26,7 @@ export const dbConfig = {
     },
   },
   production: {
-    url: JAWSDB_MARIA_URL,
+    use_env_variable: JAWSDB_MARIA_URL,
     dialect: 'mysql',
     logging: false,
     dialectOptions: {
